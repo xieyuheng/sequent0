65b1eca0f6e94f7704af15a9783f84701054656e
XIE Yuheng <xyheme@gmail.com>
2016-09-03 16:09:51 +0800

    初犯

---
+++ play.scm
@@ -0,0 +1,10 @@
+;; use chez scheme
+
+(print-graph #t)
+(include "lib/define-macro.scm")
+(include "lib/flower-barcket.scm")
+(include "lib/match.scm")
+(include "lib/helper.scm")
+(include "sequent0.scm")
+(cover-check+)
+(recur-check+)
